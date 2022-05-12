451c3a0d9f0f4b6e8ae00981b85db3fbccaf3c09
kamal <kamal@gmail.com>
2022-05-12 17:04:01 +0100

    still working on it

--- frontend/src/components/registercomp.vue
+++ frontend/src/components/registercomp.vue
@@ -3,22 +3,61 @@
        <div class="register-text"><h2>register</h2></div>
    <div class="register-inputs">
        <label><b>username <i class="fa fa-user" ></i> :</b></label>
-       <input type="text" placeholder="username" >
+       <input type="text" placeholder="username" v-model="user.fullname">
        <label><b>email <i class="fa fa-envelope" ></i> :</b></label>
-       <input type="text" placeholder="email" >
+       <input type="text" placeholder="email" v-model="user.email">
        <label><b> Password <i class="fa-solid fa-key"></i> :</b></label>
-       <input type="text" placeholder="password " >
-       <input type="submit" name="" id="" value="register" class="btn-register">
+       <input type="text" placeholder="password " v-model="user.password">
+       <input type="submit" name="" id="" value="register" class="btn-register" @click="adduser()">
    </div>
    <div><p>already have an account?</p>
-   <h3><a href="">login</a></h3>
+   <h3><a href="javascript:void(0)" >login</a></h3>
    </div>
    </div>
 
   
 </template>
 
+<script>
 
+  export default{
+      data(){
+          return{
+               users : [],
+               user : {id:'',fullname : '',email : '',password : ''},
+          }
+      },
+      methods: {
+      
+     adduser(){
+            if(this.user.fullname !== '' && this.user.email !== '' && this.user.password !== ''){
+                axios.post('http://localhost/kleider1933-website/backend/API/users/register.php',{
+                    fullname : this.user.fullname,
+                    email : this.user.email,
+                    password : this.user.password,
+                    
+                    
+                })
+                .then(response => {
+                    Swal.fire(
+                        'Added !',
+                        'success'
+                    )
+                })
+                .catch(err => console.log(err));
+            }else{
+                Swal.fire({
+                    title : 'Please fill all the fields !',
+                    type : 'warning'
+                }).then(() => {
+                    $('#adduser').modal('show')
+                })
+            }
+        }
+      }
+  }
+
+</script>
 
 <style>
    label{
